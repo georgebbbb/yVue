d7fffac4e6ea1174ba50448006f456474f49f0e1
georgebbbb <510223397@qq.com>
2016-08-29 17:55:15 +0800

    加proxy

--- one/index.js
+++ one/index.js
@@ -2,8 +2,22 @@ class Vue {
   constructor(options) {
     this.$options = options
     this._data = options.data
+    Object.keys(options.data).forEach(key => this._proxy(key))
     observer(options.data, this.$options.render)
     options.render()
+  }
+  _proxy(key) {
+    const self = this
+    Object.defineProperty(self, key, {
+      configurable: true,
+      enumerable: true,
+      get: function proxyGetter () {
+        return self._data[key]
+      },
+      set: function proxySetter (val) {
+        self._data[key] = val
+      }
+    })
   }
 }
 
@@ -33,5 +47,5 @@ var demo = new Vue({
 })
 
  setTimeout(function(){
-   demo._data.text = 444
+   demo.text = 444
  }, 3000)
